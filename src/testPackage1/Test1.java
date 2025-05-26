781ea9eff9cfe4f2872d88bb157c4634c22ab146
tushar <tusharssakhare@gmail.com>
2025-05-26 16:30:53 +0530

    First Commit

---
+++ src/testPackage1/Test1.java
@@ -0,0 +1,19 @@
+package testPackage1;
+
+public class Test1 {
+	
+	public static void main( String args[])
+	{
+		String hunger = "Yes";
+		
+		if (hunger=="Yes")
+		{
+			System.out.println("eat something...........");
+		}
+		else
+		{
+			System.out.println("do some work...........");
+		}
+	}
+
+}
