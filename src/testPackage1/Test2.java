8e3b2a2ef8b7c093deff839bf6ee53bb120d3969
tushar <tusharssakhare@gmail.com>
2025-05-26 17:59:40 +0530

    Committing Test2 to BranchX

---
+++ src/testPackage1/Test2.java
@@ -0,0 +1,21 @@
+package testPackage1;
+
+public class Test2 {
+	
+	public static void main( String args[])
+	{
+		String hunger = "Yes";
+		
+		if (hunger=="Yes")
+		{
+			System.out.println("eat something...........");
+		}
+		else
+		{
+			System.out.println("do some work...........");
+		}
+	}
+
+}
+
+
